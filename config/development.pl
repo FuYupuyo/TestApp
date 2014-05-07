6bca6a83508a3b3d52549227f479da85743b100b
fuyupuyo <fuyupuyo@localhost.localdomain>
2014-05-08 05:29:11 +0900

    memo機能と蛇足

--- config/development.pl
+++ config/development.pl
@@ -4,9 +4,7 @@ my $basedir = File::Spec->rel2abs(File::Spec->catdir(dirname(__FILE__), '..'));
 my $dbpath = File::Spec->catfile($basedir, 'db', 'development.db');
 +{
     'DBI' => [
-        "dbi:SQLite:dbname=$dbpath", '', '',
-        +{
-            sqlite_unicode => 1,
-        }
+        'dbi:mysql:amonsample', 'root', 'root',
+        +{ mysql_snable_utf8 => 1 },
     ],
 };
--- lib/TestApp/Web/Dispatcher.pm
+++ lib/TestApp/Web/Dispatcher.pm
@@ -14,6 +14,25 @@ any '/' => sub {
     });
 };
 
+get '/memo' => sub {
+    my ($c) = @_;
+    my $latest_text = $c->db->latest_text;
+    $latest_text //= "No comment";
+    return $c->render( 'memo.tx', { latest_text => $latest_text } );
+};
+
+post '/memo/insert' => sub {
+    my ($c) = @_;
+    my $text = $c->req->param('text');
+    $c->db->insert_memo($text);
+    return $c->redirect('/memo');
+};
+    
+get '/snake' => sub {
+    my ($c) = @_;
+    return $c->render( 'snake.tx', {} );
+};
+
 post '/reset_counter' => sub {
     my $c = shift;
     $c->session->remove('counter');
